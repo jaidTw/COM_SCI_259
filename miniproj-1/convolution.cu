9261002af2fabc07d9bed7d14248d67f5bbf9cdb
Hsin-Wei <hsinweiyo@gmail.com>
2021-04-19 00:48:38 -0700

    Modify convolution naive GPU version

--- miniproj-1/convolution.cu
+++ miniproj-1/convolution.cu
@@ -118,38 +118,30 @@ void convolution_layer(VTYPE synapse[Ky][Kx][Nn][Ni],
 
 
 __global__
-void GPU_convolution_layer(VTYPE synapse[Ky][Kx][Nn][Ni], 
-                           VTYPE neuron_i[NYPAD][NXPAD][Ni], 
-                           VTYPE neuron_n[NYSCL][NXSCL][Nn]) {
-  VTYPE sum[Nn]={0};
+void GPU_convolution_layer(const VTYPE synapse[Ky][Kx][Nn][Ni], 
+                           const VTYPE neuron_i[NYPAD][NXPAD][Ni], 
+                           VTYPE neuron_n[NYSCL][NXSCL][Nn],
+                           size_t pitch) {
+  // VTYPE sum[Nn]={0};
+  const int t = blockIdx.x * blockDim.x + threadIdx.x;
+  VTYPE *synapse_row = (VTYPE *)((char *)synapse + t * pitch);
+  VTYPE *neuron_i_row = (VTYPE *)((char *)neuron_i + t * pitch);
+  VTYPE *neuron_n_row = (VTYPE *)((char *)neuron_n + t * pitch);
+  VTYPE sum = 0;
+  int y = blockIdx.z / Ty + threadIdx.y;
+  int x = blockIdx.z % Tx + threadIdx.x;
 
-  //  Original code  (excluding nn, ii loops)
-  int yout = 0;
-  for (int y = 0; y < Ny; y += Sy) { // tiling for y;
-    int xout = 0;
-    for (int x = 0; x < Ny; x += Sx) { // tiling for x;
-      for (int nn = 0; nn < Nn; nn += Tn) {
-        for (int n = nn; n < nn + Tn; n++) {
-          sum[n] = 0;
-        }
-
-        // sliding window;
-        for (int ky = 0; ky < Ky; ky++)
-          for (int kx = 0; kx < Kx; kx++)
-            for (int n = nn; n < nn + Tn; n++)
-              for (int i = 0; i < Ni; i++) {
-                VTYPE sv = synapse[ky][kx][n][i];
-                VTYPE nv = neuron_i[ky + y][kx + x][i];
-                sum[n] += sv * nv;
-              }
-        for (int n = nn; n < nn + Tn; n++) {
-          neuron_n[yout][xout][n] = GPU_transfer(sum[n]);
-        }
+  // sliding window;
+  for (int ky = 0; ky < Ky; ky++) {
+    for (int kx = 0; kx < Kx; kx++) {
+      for (int i = 0; i < Ni; i++) {
+	VTYPE sv = synapse_row[blockIdx.x * SYNAPSE_SIZE + ky * (Kx * Nn * Ni) + kx * (Nn * Ni) + blockIdx.y * Ni + i];
+	VTYPE nv = neuron_i_row[blockIdx.x * (NYPAD * NXPAD * Ni) + (ky + y) * NXPAD * Ni + (kx + x) * Ni + i];
+	sum += sv*nv;
       }
-      xout++; 
     }
-    yout++;
   }
+  neuron_n_row[blockIdx.x * (NYSCL * NXSCL * Nn) + y * (NXSCL * Nn) + x * Nn + blockIdx.y]  = sum > 0 ? sum : sum/4; 
 }
 
 int main(void) {
@@ -180,15 +172,17 @@ int main(void) {
   VTYPE (*d_synapse) [Kx][Nn][Ni];
   VTYPE (*d_neuron_i) [NXPAD][Ni];
   VTYPE (*d_neuron_n) [NXSCL][Nn];
+  size_t pitch;
 
-  cudaMalloc((void **) &d_synapse, sizeof(VTYPE) * SYNAPSE_SIZE);
-  cudaMalloc((void **) &d_neuron_i, sizeof(VTYPE) * NYPAD * NXPAD * Ni);
-  cudaMalloc((void **) &d_neuron_n, sizeof(VTYPE) * NYSCL * NXSCL * Nn);
+  cudaMallocPitch((void **) &d_synapse, &pitch, sizeof(VTYPE) * Kx * Nn * Ni, Ky);
+  cudaMallocManaged((void **) &d_neuron_i, sizeof(VTYPE) * NYPAD * NXPAD * Ni);
+  cudaMallocManaged((void **) &d_neuron_n, sizeof(VTYPE) * NYSCL * NXSCL * Nn);
 
-  cudaMemcpy(d_synapse, synapse, SYNAPSE_SIZE * sizeof(VTYPE), cudaMemcpyHostToDevice); 
+  cudaMemcpy2D(d_synapse, pitch, synapse, Kx * sizeof(VTYPE), Nn * Ni  * sizeof(VTYPE), Ky,  cudaMemcpyHostToDevice); 
   cudaMemcpy(d_neuron_i, neuron_i, NYPAD * NXPAD * Ni * sizeof(VTYPE), cudaMemcpyHostToDevice); 
   cudaMemcpy(d_neuron_n, neuron_n, NYSCL * NXSCL * Nn * sizeof(VTYPE), cudaMemcpyHostToDevice); 
 
+  memset(neuron_n3, 0, NYSCL * NXSCL * Nn * sizeof(VTYPE));
 
   std::cout << "Simple version: \t";
 
@@ -197,9 +191,8 @@ int main(void) {
   
     cudaMemset(d_neuron_n, 0, NYSCL * NXSCL * Nn * sizeof(VTYPE));
     CUDA_timeit([&]() {
-      GPU_convolution_layer<<<num_blocks, num_threads>>>(d_synapse, d_neuron_i, d_neuron_n);
+      GPU_convolution_layer<<<num_blocks, num_threads>>>(d_synapse, d_neuron_i, d_neuron_n, pitch);
     });
-
     cudaMemcpy(neuron_n3, d_neuron_n, NYSCL * NXSCL * Nn * sizeof(VTYPE), cudaMemcpyDeviceToHost);
     std::cout << "#threads = " << num_threads << ", #blocks = " << num_blocks << std::endl;
     compare((VTYPE *) neuron_n, (VTYPE *) neuron_n3, NYSCL * NXSCL * Nn);

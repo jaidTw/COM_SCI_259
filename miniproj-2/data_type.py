89c4c2b2ed28ee5c1662dc05100feff3c285502c
Hsin-Wei Yu <hsinweiyo@gmail.com>
2021-05-14 02:37:46 +0800

    Before modify to DeLTA model

---
+++ miniproj-2/data_type.py
@@ -0,0 +1,18 @@
+import xlrd
+import csv
+
+FILE_NAME = 'conv.xlsx'
+
+def xlsx_to_csv():
+    workbook = xlrd.open_workbook(FILE_NAME)
+    table = workbook.sheet_by_index(0)
+    with codecs.open('gemm.csv', 'w', encoding='utf-8') as f:
+        write = csv.writer(f)
+
+        for row_num in range(table.nrows):
+            row_value = table.row_values(row_num)
+            write.writerow(row_value)
+
+
+if __name__ == '__main__':
+    xlsx_to_csv()
--- miniproj-2/mm_performance.py
+++ miniproj-2/mm_performance.py
@@ -1,6 +1,7 @@
 import numpy as np
+import csv
 
-#Maximum of TFLOPs ()
+#Maximum of TFLOPs for Titan V (13.87 TFlops)
 MAXTOPS = 14.0
 # Memory Bandwidth for Titan V (653 GB/s)
 MEM_BW = 650
@@ -9,6 +10,8 @@ MEM_CLOCK = 1.7
 # Cache maxsize for CUDA  (256 MB for Integer/ 4GB for max)
 CACHE_MAX = 256
 # L2 Cache
+
+FILENAME = 'gemm.csv'
 
 def mm_comp_time (m, n, k):
     total_ops = m * n * k
@@ -21,10 +24,27 @@ def mm_mem_time (m, n, k):
 def calculate_time (m, n, k): 
     compute_time = mm_comp_time(m, n, k)
     memory_time = mm_mem_time(m, n, k)
-    #max_value = max(compute_time, mm_time)
-    print ('%d\t%d\t%d\t' % (m, n, k), '\t%f' % compute_time, '\t%f' % memory_time, '\t%f' % max(compute_time, memory_time))
+    max_value = max(compute_time, memory_time)
+    #print ('%d\t%d\t%d\t' % (m, n, k), '\t%f' % compute_time, '\t%f' % memory_time, '\t%f' % max(compute_time, memory_time))
+    return max_value
+
+def read_parameters(file_name):
+    with open(file_name) as csv_file:
+        reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in reader:
+            if line_count == 0:
+                #print(f'{"    ".join(row)}')
+                print('m\tn\tk\tDeepBench Time\t Predicted Time')
+                line_count += 1
+            time = calculate_time(int(row["m"]), int(row["n"]), int(row["k"]))
+            print(f'{row["m"]}\t{row["n"]}\t{row["k"]}\t{row["time"]}\t', '\t%f' % time)
+            line_count += 1
+        print(f'Processed {line_count} lines.')
 
 if __name__ == '__main__':
+    read_parameters(FILENAME)
+    '''
     print ('m\tn\tk\tcomputation bound\tmemory bound\tpredicted time')
     calculate_time(5124,  700, 2048)
     calculate_time(  35,  700, 2048)
@@ -34,4 +54,4 @@ if __name__ == '__main__':
     calculate_time(  35, 1500, 2048)
     calculate_time(5124, 1500, 2560)
     calculate_time(  35, 1500, 2560)
-    # mm_mem_time(1760, 16, 1760)
+    '''# mm_mem_time(1760, 16, 1760)
